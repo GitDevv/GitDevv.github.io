dab0b2d6f584af1d59a57c58dd336ffb47adbbd4
Vitaliy <solutionrise@ukr.net>
2018-03-06 16:32:38 +0200

    Add files via upload

---
+++ ARview/script360.js
@@ -0,0 +1,30 @@
+var vrView;
+ 
+function onLoad() {
+  vrView = new VRView.Player('#vrview', {
+	width:'100%',
+	height:480,
+    image: '360-view.jpg',
+    preview: '360-view.jpg',
+    is_stereo: true,
+    is_autopan_off: true
+  });
+
+  vrView.on('ready', onVRViewReady);
+  vrView.on('modechange', onModeChange);
+  vrView.on('error', onVRViewError);
+}
+
+function onVRViewReady(e) {
+  console.log('onVRViewReady');
+}
+
+function onModeChange(e) {
+  console.log('onModeChange', e.mode);
+}
+
+function onVRViewError(e) {
+  console.log('Error! %s', e.message);
+}
+
+window.addEventListener('load', onLoad);
