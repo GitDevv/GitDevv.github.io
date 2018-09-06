a08f5a6508221e5d63eccf01d36385e151f36256
Vitaliy <solutionrise@ukr.net>
2018-09-06 11:11:45 +0300

    Add files via upload

---
+++ GORE/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){	
+    
+    $('.btn-right').click(function(e){
+        e.preventDefault();
+        $('.right-modal').fadeIn();
+    });
+    
+    $('.btn-false').click(function(e){
+        e.preventDefault();
+        $('.false-modal').fadeIn();
+    });    
+ 
+}); 
+ 
